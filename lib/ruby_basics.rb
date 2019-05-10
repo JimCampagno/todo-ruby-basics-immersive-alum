b322db5042b61e481e0cd8f6bacb5d2e671d65d4
lilaro <larochelle.lisa@gmail.com>
2019-05-10 16:03:07 -0400

    give methods desired behavior, as defined by tests

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,27 +1,28 @@
 def division(num1, num2)
- 
+ num1 / num2
 end
 
 def assign_variable(value)
-  
+  value = "Bob"
 end
 
-def argue
-  
+def argue(argument = "I'm right. You're wrong.")
+  argument
 end
 
-def greeting
-
+def greeting(greeting, name)
+  greeting = "Oh hello, "
+  name = "Lisa!"
 end
 
-def return_a_value
-
+def return_a_value(value = "Nice")
+  value
 end
 
 def last_evaluated_value
-
+  last_evaluated_value = "expert"
 end
 
-def pizza_party
-
+def pizza_party(topping = "cheese")
+  topping
 end
